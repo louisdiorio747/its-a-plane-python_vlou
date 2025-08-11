e4ef7c0dd18ef3a55b6bc6a3fe658ce8cf3901cc
louisdiorio747 <33705794+louisdiorio747@users.noreply.github.com>
2025-08-10 22:06:44 -0400

    Update flightdetails.py

--- scenes/flightdetails.py
+++ scenes/flightdetails.py
@@ -11,7 +11,7 @@ FLIGHT_NO_POSITION = (120, 21)
 FLIGHT_NO_TEXT_HEIGHT = 8  # based on font size
 FLIGHT_NO_FONT = fonts.small
 
-ORIGIN_CITY_POSITION = (40, 21)
+ORIGIN_CITY_POSITION = (35, 21)
 DESTINATION_CITY_POSITION = (256, 21)
 CITY_TEXT_HEIGHT = 8  # based on font size
 CITY_FONT = fonts.extrasmall
